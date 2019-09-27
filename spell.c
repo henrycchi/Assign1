7daa747190adc98aa59b47cd767deda10cf40740
henrycchi <hcc2045@nyu.edu>
2019-09-27 15:50:50 -0400

    Update to spell.c for testing Val

--- spell.c
+++ spell.c
@@ -5,98 +5,181 @@ CS-GY 9163
 Assignment #1
 */
 
-
-#include <stdlib.h>
-#include <stdio.h>
+#include "dictionary.h"
 #include <string.h>
 #include <ctype.h>
-#include "dictionary.h"
+#include <stdlib.h>
 
-// Turns all letters to upper case using toupper function.
+//nonvalue-returning function for changing case to lowercase
 
-void Uppercase(char * str) {
-	if (str != NULL) {
-		int len = strlen(str);
-		for (int place = 0; place < len; place++) {
-			str[place] = toupper(str[place]);
+void Lowercase(char * lowstring) {
+	if (lowstring != NULL) {
+		int len = strlen(lowstring);
+		for (int i = 0; i < len; i++) {
+			lowstring[i] = tolower(lowstring[i]);
 		}
 	}
 }
 
-// Function 1 -  A function to load the list of words into the hash map (called load_dictionary).  Returns true if successful else false.
+// Function 1 -  A function to load the list of words into the bucket map (called load_dictionary).  Returns true if successful else false.
 
-bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[]) {
-  FILE* wordlist;
-  wordlist = fopen(dictionary_file, "r");
-  if (wordlist == NULL) {
-      return false;
-  }
-  for(int i = 0; i < HASH_SIZE; i++) {
-      hashtable[i] = NULL;
-  }
-  char buff[LENGTH+1];
-  while (fscanf(wordlist, "%s", buff) > 0) {
-      Uppercase(buff);
-      node* new_node = malloc(sizeof(node));
-      new_node->next = NULL;
-      strcpy(new_node->word, buff);
-      int hash = hash_function(new_node->word);
-      if (hashtable[hash] == NULL) {
-          hashtable[hash] = new_node;
-      }
-      else {
-          new_node->next = hashtable[hash];
-          hashtable[hash] = new_node;
-      }
-  }
-  fclose(wordlist);
-  return true;
+/**
+function load_dictionary(string dictionary, hashmap hashtable[])
+{
+    Initialize all values in bucket table to NULL.
+    Open dict_file from path stored in dictionary.
+    If dict_file is NULL:
+        return false.
+    While word in dict_file is not EOF (end of file):
+        Set hashmap_t new_node to a new node.
+        Set new_node->next to NULL.
+        Set new_node->word equal to word.
+        Set int bucket to hash_function(word).
+        if hashtable[bucket] is NULL:
+            Set hashtable[bucket] to new_node.
+        else:
+            Set new_node->next to hashtable[bucket].
+            Set hashtable[bucket] to new_node.
+    Close dict_file.
 }
+*/
+
+bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[]){   
+	FILE *dict_file = fopen(dictionary_file, "r");
+	if (!dict_file) {
+		return false;
+	}
+	else {
+        	for(int i = 0; i < HASH_SIZE; i++) {
+      		hashtable[i] = NULL;
+		}
+        	int bucket;
+        	char line[LENGTH];
+        	while(fgets(line,LENGTH,dict_file)){
+         		if(line[strlen(line)-1] =='\n') {
+				line[strlen(line)-1]='\0';
+			}
+            		Lowercase(line);
+       			bucket = hash_function(line);
+            	if(hashtable[bucket] == NULL){
+                    node * cursor = malloc(sizeof(node));
+                    strncpy(cursor->word,line,LENGTH);
+                    cursor->next = NULL;
+                    hashtable[bucket] = cursor;
+            	}
+            	else {
+                    node * cursor = malloc(sizeof(node));
+                    strncpy(cursor->word,line,LENGTH);
+                    cursor->next =hashtable[bucket];
+                    hashtable[bucket] = cursor;
+                }
+        }
+        fclose(dict_file);
+        return true;
+        }
+    }
 
 //Function 2 -  Function called check_words to tie them all together
 
-int check_words(FILE* fp, hashmap_t hashtable[], char * misspelled[]) {
-	int WrongCount = 0;
-        char line[4096];
-        while(fgets(line,4096,fp)){
-        char * token = strtok(line," .,-!?\n");
-        while(token!= NULL){
-            if (!check_word(token, hashtable)){
-                misspelled[WrongCount] = token;
-                WrongCount++;
-            }
-            token = strtok(NULL," .,-!?\n");
-        }
-    }
-    return WrongCount;
+/**
+function check_words(file fp, hashmap hashtable[], string misspelled[])
+{
+    Set int num_misspelled to 0.
+    While line in fp is not EOF (end of file):
+        Read the line.
+        Split the line on spaces.
+        For each word in line:
+            Remove punctuation from beginning and end of word.
+            If not check_word(word):
+                Append word to misspelled.
+                Increment num_misspelled.
+    Return num_misspelled.
 }
-//Function 3 - A function to check if a word is correctly spelled returns TRUE or FALSE
+*/
 
-bool check_word(const char* word, hashmap_t hashtable[]) {
-	if ((word == NULL) || (hashtable == NULL)) {
-		return false; // NULL values not to be used
+int check_words(FILE* fp, hashmap_t hashtable[], char* misspelled[]){ 
+	if(!fp) {
+		return false;
 	}
-	char UpperWord[strlen(word)]; 
-	sprintf(UpperWord, "%s", word); //Stored as a string in the buffer 
-	Uppercase(UpperWord);
-	int hash = hash_function(UpperWord);
-	hashmap_t hashes = hashtable[hash];
-	if (hashes == NULL)  {
+    	else {
+    		int num_misspelled = 0 ;
+		char BuffW[LENGTH];
+		char* placeholder = strtok(BuffW," ");
+		while(fgets(BuffW,LENGTH,fp) != NULL) {
+			placeholder = strtok(NULL," ");
+        		while(placeholder != NULL) {
+            			if(placeholder[strlen(placeholder)-1] =='\n') {
+					placeholder[strlen(placeholder)-1]='\0';
+				}
+				placeholder[strlen(placeholder)]='\0';           
+         			bool punctright = ispunct(placeholder[strlen(placeholder)-1]);
+            			if(punctright) {
+					while(punctright) {
+                			placeholder[strlen(placeholder)-1]='\0';
+			                punctright = ispunct(placeholder[strlen(placeholder)-1]);
+                    			}
+                		}
+				bool punctleft = ispunct(placeholder[0]);
+         	       		if(punctleft) {
+					while(punctleft) {
+                       				placeholder++;
+                    				punctleft = ispunct(placeholder[0]);
+                    			}
+                		}
+				bool wordCheckResult = check_word(placeholder,hashtable);
+            			if(!(wordCheckResult == 1 && strlen(BuffW) < LENGTH)) {
+                			misspelled[num_misspelled] = malloc(strlen(placeholder));
+                			strcpy(misspelled[num_misspelled], placeholder);
+                			num_misspelled ++;
+                		}
+            			placeholder = strtok(NULL," ");
+            		}
+           	return num_misspelled;
+        	}
+    	}
+        return true;
+}
+
+//Function 3 - A function to check if a word is correctly spelled returns TRUE or FALSE
+/**
+fuction check_word(string word, hashmap hashtable[])
+{
+    Set int bucket to the output of hash_function(word).
+    Set hashmap_t cursor equal to hashmap[bucket].
+    While cursor is not NULL:
+        If word equals cursor->word:
+            return True.
+        Set curosr to cursor->next.
+    Set int bucket to the output of hash_function(word).
+    Set hashmap_t cursor equal to hashmap[bucket].
+    While cursor is  not NULL:
+        If lower_case(word) equals curosr->word:
+            return True.
+        Set curosr to cursor->next.
+    return False.
+}
+*/
+bool check_word(const char* word, hashmap_t hashtable[])
+{
+	char LowerWord[strlen(word)]; 
+	sprintf(LowerWord, "%s", word); //Stored as a string in the buffer 
+	Lowercase(LowerWord);
+	int bucket = hash_function(LowerWord);
+	hashmap_t temp = hashtable[bucket];
+	if (temp == NULL)  {
 		return false; // NO MATCH
 	}
-	bool CorrectSpell = false;
-	hashmap_t currentNode = hashes;
-	while (currentNode->next != NULL) {
-		if (strncmp(UpperWord, currentNode->word, strlen(currentNode->word)) == 0) {
-			CorrectSpell = true;
+	bool Returned = false;
+	hashmap_t cursor = temp;
+	while (cursor->next != NULL) {
+		if (strncmp(LowerWord, cursor->word, strlen(cursor->word)) == 0) {
+			Returned = true;
 			break;
 	        } 
 		else {
-			CorrectSpell = false;		
+			Returned = false;		
 		}
-		currentNode = currentNode->next;
+		cursor = cursor->next;
 	}
-	return CorrectSpell;
+	return Returned;
 }
-
-
